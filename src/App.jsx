69471824e5f9162a85004f3187911db19f944312
muhyasin89 <muhyasin89@gmail.com>
2024-03-31 20:10:42 +0700

    [main]add react three js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useFrame } from "@react-three/fiber"
+import {useRef} from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Cube = ({position, size, color}) => {
+    const ref = useRef();
+    useFrame((state, delta) => {
+      ref.current.rotation.x += delta;
+    })
+
+    return (
+      <mesh position={position}>
+          <boxGeometry args={size}/>
+          <meshBasicMaterial color={color}/>
+        </mesh>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas>
+        <directionalLight position={[0, 0, 2]} />
+        <ambientLight />
+
+        <Cube position={[1, 0, 0]} color={"green"} size={[1, 1, 1]} intensity={0.5}/>
+        <Cube position={[-1, 0, 0]} color={"hotpink"} size={[1, 1, 1]} intensity={0.5}/>
+
+        <Cube position={[-1, 2, 0]} color={"blue"} size={[1, 1, 1]} intensity={0.5}/>
+        <Cube position={[1, 2, 0]} color={"yellow"} size={[1, 1, 1]} intensity={0.5}/>
+      </Canvas>
     </>
   )
 }
 
 export default App
+ 
